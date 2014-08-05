0f7a2d6acc58ba64593515b4a9ec12ae18f12b74
Hoan Cai <hoancai@Hoan-Cais-MacBook-Pro.local>
2014-08-04 22:37:45 -0700

    Dynamic Home Page

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,10 +30,10 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: ENV["DOMAIN_NAME"]
-    authentication: "plain"
-    enable_starttls_auto: "true"
-    user_name: ENV["GMAIL_USERNAME"]
+    domain: ENV["DOMAIN_NAME"],
+    authentication: "plain",
+    enable_starttls_auto: "true",
+    user_name: ENV["GMAIL_USERNAME"],
     password: ENV["GMAIL_PASSWORD"]
 
   }
