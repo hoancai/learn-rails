b7f43210e88451527c24631ddc2ca66a5d36545d
Hoan Cai <hoancai@Hoan-Cais-MacBook-Pro.local>
2014-09-07 00:56:57 -0700

    changed picture

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -2,9 +2,27 @@ class VisitorsController < ApplicationController
 
 	def new
 
-		@owner = Owner.new
+		@visitor = Visitor.new
 
 
+	end
+
+
+	def create
+		@visitor = Visitor.new(secure_params)
+		if @visitor.valid?
+			@visitor.suscribe
+			flash[:notice] = "Youve signed up with #{@visitor.email}."
+			redirect_to root_path
+		else
+			render :new
+		end
+	end
+
+
+	private 
+	def secure_params
+		params.required(:visitor).permit(:email)
 	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   # root 'welcome#index'
 
   resources :contacts, only: [:new,:create]
+  resources :visitors, only: [:new, :create]
   root to: 'visitors#new'
 
   # Example of regular route:
